fdcae262138f8e49e784a065143825acb233961a
vika41130 <vika41130@gmail.com>
2021-01-13 11:41:43 +0700

    [list-scroll-vertical]

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,53 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String message = "";
+  ScrollController _controller;
+  final itemSize = 100.0;
+
+  @override
+  void initState() {
+    _controller = ScrollController();
+    _controller.addListener(() {
+      _scrollListener();
+    });
+    super.initState();
+  }
+
+  _scrollListener() {
+    print('------------------------_scrollListener run');
+    if (_controller.offset >= _controller.position.maxScrollExtent &&
+        !_controller.position.outOfRange) {
+      setState(() {
+        message = "reach the bottom";
+      });
+    }
+    if (_controller.offset <= _controller.position.minScrollExtent &&
+        !_controller.position.outOfRange) {
+      setState(() {
+        message = "reach the top";
+      });
+    }
+  }
+
+  _moveUp() {
+  // _controller.jumpTo(pixelsToMove);
+    _controller.animateTo(_controller.offset - itemSize,
+        curve: Curves.linear, duration: Duration(milliseconds: 500));
+  }
+
+  _moveDown() {
+    _controller.animateTo(_controller.offset + itemSize,
+        curve: Curves.linear, duration: Duration(milliseconds: 500));
+  }
+
+
   @override
   Widget build(BuildContext context) {
     final title = 'Flutter Demo';
@@ -17,39 +63,38 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text(title),
         ),
-        body: SafeArea(
-          child: Container(
-            width: double.infinity,
-            child: Stack(
-              children: [
-                Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Flexible(
-                          // flex: 1,
-                          // fit: FlexFit.tight,
-                          child: Container(
-                            padding: EdgeInsets.only(top: 20, bottom: 20),
-                            child: Text(
-                              'Development',
-                              style: TextStyle(
-                                fontSize: 50,
-                              ),
-                            ),
-                            decoration: BoxDecoration(
-                              border: Border(bottom: BorderSide(color: Colors.black, width: 2),),
-                            ),
-                          ),
-                        ),
-                      ],
+        body: Column(
+          children: <Widget>[
+            Container(
+              height: 50.0,
+              color: Colors.green,
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    RaisedButton(
+                      child: Text("up"),
+                      onPressed: _moveUp,
                     ),
+                    RaisedButton(
+                      child: Text("down"),
+                      onPressed: _moveDown,
+                    )
                   ],
-                )
-              ],
+                ),
+              ),
             ),
-          ),
+            Expanded(
+              child: ListView.builder(
+                controller: _controller,
+                itemCount: 30,
+                itemExtent: itemSize,
+                itemBuilder: (context, index) {
+                  return ListTile(title: Text("Index : $index"));
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
