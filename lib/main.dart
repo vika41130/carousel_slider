de678981d510e5f89a3ace77ebf1b2c14a3a3993
vika41130 <vika41130@gmail.com>
2021-01-14 14:31:09 +0700

    [list-scroll-vertical] Expanded

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,42 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     final title = 'Flutter Demo';
+    final List<Widget> tempItems = [];
+    for (var i = 0; i < 30; i++) {
+      if (i == 15) {
+        tempItems.add(Container(
+          height: 150,
+          // child: Text('Index: $i Overflow'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Image(image: AssetImage('assets/images/bmw/driver1.jpg'),),
+              Text('Index: $i Overflow'),
+              Text('Value'),
+            ],
+          ),
+          decoration:
+              BoxDecoration(
+              // border: Border.all(color: Colors.black, width: 2),
+              color: Colors.blue,
+            ),
+        ));
+      } else {
+        tempItems.add(Container(
+          height: 100,
+          // child: Image(image: AssetImage('assets/images/bmw/driver1.jpg'),),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Image(image: AssetImage('assets/images/bmw/driver1.jpg'),),
+            ],
+          ),
+          decoration:
+            BoxDecoration(
+            ),
+        ));
+      }
+    }
     return MaterialApp(
       title: title,
       theme: ThemeData(
@@ -31,10 +67,31 @@ class _MyAppState extends State<MyApp> {
           body: Column(
             children: [
               Container(
-                height: 100,
+                height: 140,
+              ),
+              Expanded(
                 child: SwiperWidget(
                   itemWidth: 200,
+                  itemMargin: 20,
+                  items: tempItems,
+                  numbersOfItemsVisible: 3,
+                  preBottom: 10,
+                  preLeft: 10,
+                  nextBottom: 10,
+                  nextRight: 10,
                 ),
+              ),
+              // Container(
+              //   height: 100,
+              //   child: SwiperWidget(
+              //     itemWidth: 200,
+              //     itemMargin: 20,
+              //     items: tempItems,
+              //     numbersOfItemsVisible: 3,
+              //   ),
+              // ),
+              Expanded(
+                child: Text('bottom bar'),
               ),
             ],
           )),
--- lib/widget/swiper_widget.dart
+++ lib/widget/swiper_widget.dart
@@ -2,21 +2,41 @@ import 'package:flutter/material.dart';
 
 class SwiperWidget extends StatefulWidget {
   final double itemWidth;
+  final double itemMargin;
+  final int numbersOfItemsVisible;
+  final Widget preButton;
+  final Widget nextButton;
+  final double preLeft;
+  final double preBottom;
+  final double nextRight;
+  final double nextBottom;
+  final List<Widget> items;
 
   SwiperWidget({
     Key key,
-    this.itemWidth,
+    @required this.itemWidth,
+    this.itemMargin,
+    this.numbersOfItemsVisible,
+    this.preButton,
+    this.nextButton,
+    this.nextBottom,
+    this.nextRight,
+    this.preBottom,
+    this.preLeft,
+    @required List<Widget> items,
   })
-    : super(key: key);
+    : items = items ?? []
+    , super(key: key);
   @override
   _SwiperWidgetState createState() => _SwiperWidgetState();
 }
 
 class _SwiperWidgetState extends State<SwiperWidget> {
-  String message = "";
   ScrollController _controller;
   bool isBeginned = true;
   bool isEnded = false;
+  double marginPerDirection = 0;
+  Size widgetSize;
 
   @override
   void initState() {
@@ -25,10 +45,24 @@ class _SwiperWidgetState extends State<SwiperWidget> {
       _scrollListener();
     });
     super.initState();
+    prepareData();
+  }
+
+  void prepareData() {
+  }
+
+  void calculateMargin() {
+    setState(() {
+      if (widget.numbersOfItemsVisible == null || widget.numbersOfItemsVisible == 0) {
+        marginPerDirection = 0;
+      } else {
+        marginPerDirection = (widgetSize.width - widget.itemWidth * widget.numbersOfItemsVisible) / 2;
+      }
+    });
   }
 
   _scrollListener() {
-    print('------------------reset_scrollListener run');
+    // print('------------------reset_scrollListener run');
     setState(() {
       isBeginned = false;
       isEnded = false;
@@ -36,14 +70,12 @@ class _SwiperWidgetState extends State<SwiperWidget> {
     if (_controller.offset >= _controller.position.maxScrollExtent &&
         !_controller.position.outOfRange) {
       setState(() {
-        message = "reach the end";
         isEnded = true;
       });
     }
     if (_controller.offset <= _controller.position.minScrollExtent &&
         !_controller.position.outOfRange) {
       setState(() {
-        message = "reach the begin";
         isBeginned = true;
       });
     }
@@ -62,57 +94,62 @@ class _SwiperWidgetState extends State<SwiperWidget> {
 
   @override
   Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      setState(() {
+        widgetSize = context.size;
+        // print('------------------------------widgetSize width-height: ${widgetSize.width}-${widgetSize.height}');
+        calculateMargin();
+      });
+    });
     return Stack(
-      children: <Widget>[
-        SizedBox(
-          width: 30,
-        ),
-        Container(
-          margin: EdgeInsets.only(left: 200, right: 200),
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            controller: _controller,
-            itemCount: 30,
-            itemExtent: widget.itemWidth,
-            itemBuilder: (context, index) {
-              return Container(
-                child: Text('Index : $index'),
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black, width: 2)),
-              );
-            },
+        children: <Widget>[
+    Container(
+      margin: EdgeInsets.only(left: marginPerDirection, right: marginPerDirection),
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        controller: _controller,
+        itemCount: widget.items.length,
+        itemExtent: widget.itemWidth,
+        itemBuilder: (context, index) {
+          return OverflowBox(
+                      child: Container(
+                        height: 100,
+              margin: EdgeInsets.only(right: index == widget.items.length - 1 ? 0 : widget.itemMargin),
+              child: widget.items[index],
+            ),
+          );
+        },
+      ),
+    ),
+    isBeginned
+        ? SizedBox()
+        : Positioned(
+            // top: widgetSize != null ? widgetSize.height / 2 : 0,
+            bottom: widget.preBottom ?? 0,
+            left: widget.preLeft ?? 0,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RaisedButton(
+                child: Icon(Icons.arrow_left),
+                onPressed: _movePre,
+              ),
+            ),
           ),
-        ),
-        SizedBox(
-          width: 30,
-        ),
-        isBeginned
-            ? SizedBox()
-            : Positioned(
-                bottom: 0,
-                left: 0,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: RaisedButton(
-                    child: Text("pre"),
-                    onPressed: _movePre,
-                  ),
-                ),
+    isEnded
+        ? SizedBox()
+        : Positioned(
+            // top: widgetSize != null ? widgetSize.height / 2 : 0,
+            bottom: widget.nextBottom ?? 0,
+            right: widget.nextRight ?? 0,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RaisedButton(
+                child: Icon(Icons.arrow_right),
+                onPressed: _moveNext,
               ),
-        isEnded
-            ? SizedBox()
-            : Positioned(
-                bottom: 0,
-                right: 0,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: RaisedButton(
-                    child: Text("next"),
-                    onPressed: _moveNext,
-                  ),
-                ),
-              ),
-      ],
-    );
+            ),
+          ),
+        ],
+      );
   }
 }
