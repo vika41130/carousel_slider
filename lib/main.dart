0b2c1b97e1955c35df7c4781277144600b888997
vika41130 <vika41130@gmail.com>
2021-04-02 16:48:47 +0700

    [vertical_card_pager]

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:vertical_card_pager/vertical_card_pager.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
+
+  final List<Widget> images = [
+    Container(
+      color: Colors.red,
+    ),
+    Container(
+      color: Colors.yellow,
+    ),
+    Container(
+      color: Colors.black,
+    ),
+    Container(
+      color: Colors.cyan,
+    ),
+    Container(
+      color: Colors.blue,
+    ),
+    Container(
+      color: Colors.grey,
+    ),
+  ];
+  final List<String> titles = List.generate(6, (index) => index.toString());
+
+
   @override
   Widget build(BuildContext context) {
-    final title = 'Flutter Demo';
+    final title = 'Flutter Testing';
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: title,
@@ -20,35 +46,26 @@ class MyApp extends StatelessWidget {
         ),
         body: SafeArea(
           child: Container(
-            width: double.infinity,
-            child: Stack(
-              children: [
-                Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Flexible(
-                          // flex: 1,
-                          // fit: FlexFit.tight,
-                          child: Container(
-                            padding: EdgeInsets.only(top: 20, bottom: 20),
-                            child: Text(
-                              'Development',
-                              style: TextStyle(
-                                fontSize: 50,
-                              ),
-                            ),
-                            decoration: BoxDecoration(
-                              border: Border(bottom: BorderSide(color: Colors.black, width: 2),),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                )
-              ],
+            height: 400,
+            child: Column(
+              children: <Widget>[
+              Expanded(
+                child: Container(
+                  child: VerticalCardPager(
+                    textStyle: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                    titles: titles,
+                    images: images,
+                    onPageChanged: (page) {
+                      print('-------------------------------------onPageChanged ${page}');
+                    },
+                    align: ALIGN.CENTER,
+                    onSelectedItem: (index) {
+                      print('-------------------------------------onSelectedItem ${index}');
+                    },
+                  ),
+                ),
+              ),
+            ],
             ),
           ),
         ),
