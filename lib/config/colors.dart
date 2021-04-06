85b55d6838019dd2ff7548a29c9355970e42743a
vika41130 <vika41130@gmail.com>
2021-04-06 15:49:31 +0700

    percent_indicator

---
+++ lib/config/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Cl {
+  Cl._();
+
+  static const green_light = Color(0xFF1597bb);
+}
--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,49 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:lutoi/config/colors.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  double percent = 0;
+  Timer _time;
+
+  @override
+  void initState() {
+    super.initState();
+    // _time = Timer.periodic(Duration(seconds: 2), (timer) {
+    //   percent += 0.1;
+    //   if (percent > 1.0) {
+    //     percent = 1.0;
+    //     _time.cancel();
+    //     return;
+    //   }
+    //   setState(() {});
+    // });
+    // 
+    // WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+    //   for (var i = 0; i < 10; i++) {
+    //     Future.delayed(Duration(seconds: 1), () {
+    //       percent += 0.1;
+    //       setState(() {});
+    //     });
+    //   }
+    // });
+    
+  }
+
+  @override
+  void dispose() {
+    _time.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final title = 'Flutter Demo';
@@ -11,15 +53,38 @@ class Home extends StatelessWidget {
         ),
         body: SafeArea(
           child: Container(
+            margin: EdgeInsets.all(10),
             child: Column(
               children: [
                 Spacer(),
+                RaisedButton(
+                  onPressed: () {
+                    if (percent >= 1.0) {
+                      return;
+                    }
+                    percent += 0.1;
+                    setState(() {});
+                  },
+                  child: Text('Incre Percent'),
+                ),
+                CircularPercentIndicator(
+                  radius: 120,
+                  lineWidth: 15,
+                  percent: percent,
+                  progressColor: Colors.green,
+                  backgroundColor: Colors.red,
+                  // animation: true,
+                  // animationDuration: 1000,
+                  header: Text('Header',),
+                  center: Text("100%"),
+                  footer: Text('Footer'),
+                ),
+                SizedBox(height: 10,),
                 LinearPercentIndicator(
-                  width: MediaQuery.of(context).size.width,
                   lineHeight: 14.0,
-                  percent: 0.5,
+                  percent: 0.8,
                   backgroundColor: Colors.grey,
-                  progressColor: Colors.blue,
+                  progressColor: Colors.cyan[500],
                 ),
               ],
             ),
